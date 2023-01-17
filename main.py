452217b559a16a57856f177969307f8d3a197268
s20501 <s20501@pjwstk.edu.pl>
2023-01-17 17:39:07 +0100

    Generowanie wykresu

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import math
 
+import matplotlib.pyplot as plt
+
+
+def frf(f, c, r):
+    # Przygotowanie danych pod wykres
+    c1, c2, r1, r2 = c, c, r, r
+    # Częstość graniczna,
+    w = 2 * math.pi * f
+    num = 1 / (c1 * c2 * r1 * r2)
+    den = 1 / (c1 * c2 * r1 * r2) + (r1 + r2) / (c1 * r1 * r2) * 1j * w - w ** 2
+    return num / den
+
+
+def dB(x):
+    # Zwraca wartość w decybelach
+    return 20 * math.log10(abs(x))
+
+
+# Główna pętla programu
 if __name__ == '__main__':
     # Pobieram dane wejściowe
     fc = float(input("fc(Hz)= "))
@@ -25,3 +44,23 @@ if __name__ == '__main__':
     print('C1 = C2 = 100nF')
     print(f'R3 = {int(r3)}Ω')
     print(f'R4 = 10000Ω')
+
+    plt.figure()
+
+    target = (fc, dB(frf(fc, c, r)))
+
+    freqs = range(1000000)  # zakres 0 Hz do 1 MHz
+    response = [dB(frf(f, c, r)) for f in freqs]
+
+    ax = plt.subplot(2, 1, 1)
+    plt.semilogx(freqs, response)
+    plt.semilogx(target[0], target[1], ms=10)
+    plt.title('LPF')
+    plt.xlabel('[Hz]')
+    plt.ylabel('[dB]')
+
+    lims = list(ax.get_ylim())
+    lims[1] = 20
+    plt.ylim(lims)
+
+    plt.show()
