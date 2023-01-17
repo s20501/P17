5b49471797c92caa4a4e0b070b4bbfcfa21b0bc6
s20501 <s20501@pjwstk.edu.pl>
2023-01-17 18:30:54 +0100

    Wykres generowanie

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import matplotlib.pyplot as plt
 def frf(f, c, r):
     # Przygotowanie danych pod wykres
     c1, c2, r1, r2 = c, c, r, r
-    # Częstość graniczna,
     w = 2 * math.pi * f
+
+    # Częstość graniczna,
     num = 1 / (c1 * c2 * r1 * r2)
     den = 1 / (c1 * c2 * r1 * r2) + (r1 + r2) / (c1 * r1 * r2) * 1j * w - w ** 2
+
     return num / den
 
 
@@ -47,15 +49,17 @@ if __name__ == '__main__':
 
     plt.figure()
 
-    target = (fc, dB(frf(fc, c, r)))
+    # Zakres 0 Hz do 1 MHz
+    freqs = range(1000000)
 
-    freqs = range(1000000)  # zakres 0 Hz do 1 MHz
+    # Przygotowywanie danych pod wykres
+    target = (fc, dB(frf(fc, c, r)))
     response = [dB(frf(f, c, r)) for f in freqs]
 
+    # Generowanie wykresu
     ax = plt.subplot(2, 1, 1)
     plt.semilogx(freqs, response)
     plt.semilogx(target[0], target[1], ms=10)
-    plt.title('LPF')
     plt.xlabel('[Hz]')
     plt.ylabel('[dB]')
 
